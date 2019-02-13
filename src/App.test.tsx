b0e94d7fe2a783ba58b0a51aa37988966cbf2e3c
JulianG <juliangaramendy@gmail.com>
2019-02-13 10:13:52 +0000

    !

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,16 +4,16 @@ import { render, fireEvent, wait } from "react-testing-library";
 it("must be a problem between the chair and the keyboard", async () => {
   const div = document.createElement("div");
 
-  const { getByValue, debug } = render(<input type="text" value="initial text" />);
+  const { getByValue, debug } = render(
+    <input type="text" value="initial text" />
+  );
 
   const input = getByValue(/initial text/i); // ok
-  
-  fireEvent.change(input, { target: { value: 'updated text' } }); // ok
-  
-  // text input still contains the initial value
-  await wait(()=> getByValue(/updated text/i)); // ❌
- 
-});
 
+  fireEvent.change(input, { target: { value: "updated text" } }); // ok
+
+  // text input still contains the initial value
+  await wait(() => getByValue(/updated text/i)); // ❌
+});
 
 
