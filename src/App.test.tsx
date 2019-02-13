9238e63a683bde3d6c0620e194ab48fdcd0b054e
JulianG <juliangaramendy@gmail.com>
2019-02-13 13:15:08 +0000

    avoiding rendering App.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,16 +4,18 @@ import { render, fireEvent, wait } from "react-testing-library";
 it("must be a problem between the chair and the keyboard", async () => {
   const div = document.createElement("div");
 
+  const onChange = jest.fn();
+
   const { getByValue, debug } = render(
-    <input type="text" value="initial text" />
+    <input type="text" value="initial text" onChange={onChange} />
   );
 
   const input = getByValue(/initial text/i); // ok
 
   fireEvent.change(input, { target: { value: "updated text" } }); // ok
 
+  expect(onChange).toHaveBeenCalled() // ok
+
   // text input still contains the initial value
   await wait(() => getByValue(/updated text/i)); // ❌
 });
-
-
